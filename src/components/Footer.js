cb2ac6a9a769dc82d24b792f68aaa6e8d033a1e2
Supriya Kankure <supriyakankure@Supriyas-MacBook-Air.local>
2023-01-26 23:00:16 -0500

    code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,33 +3,35 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 
 function SocialMediaIcons(){
-    return<ul className="list-unstyled social-media-links d-flex">
-          <li><i className="bi bi-twitter social-media-icon" /></li>
-          <li><i className="bi bi-instagram social-media-icon" />{" "}</li> 
-          <li><i className="bi bi-facebook social-media-icon" /></li>
-          <li><a className="footer-anchor-links" href="mailto:info@jalebijodi.com"> <i className="bi bi-envelope social-media-icon" /></a></li>
-          {/* <i class="bi bi-tiktok"></i> */}
+    return<ul className="social-media-links list-unstyled justify-content-center d-flex">
+          <li><i className="bi bi-twitter social-media-icon"/></li>
+          <li><i className="bi bi-instagram social-media-icon"/></li> 
+          <li><i className="bi bi-facebook social-media-icon"/></li>
+          <li><a className="" href="mailto:info@jalebijodi.com"> <i className="bi bi-envelope social-media-icon" /></a></li>
           </ul>
 }
 
 function FooterLinks(){
-  return <ul className="list-inline text-center footer-font">
+  return <div className = 'footer-links d-flex justify-content-center'>
+    <ul className="footer-links list-inline text-center footer-font">
          <li className="list-inline-item resources-links"><a>Contact</a></li>
          <li className="list-inline-item resources-links footer-anchor-links"><a href="privacy-policy.html">Privacy Policy</a></li>
          <li className="list-inline-item resources-links footer-anchor-links"><a href="terms.html">Terms of Use</a></li>
-        </ul>
+    </ul>
+  </div>
 }
 
 function CopyRight(){
-  return <p className="footer-font-2">© 2023 Jalebi Jodi. All rights reserved.</p>
+  return <p className="copyright d-flex justify-content-center">© 2023 Jalebi Jodi. All rights reserved.</p>
 }
 
 const Footer = () => {
   return (
-    <Container className='custom-footer box-shadow'>
+    <Container className='footer-container box-shadow'>
       <Row className="pt-5 px-5 g-0">
       <SocialMediaIcons></SocialMediaIcons>
       <FooterLinks></FooterLinks>
+      <CopyRight></CopyRight>
       </Row>
     </Container>
   )
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,46 +1,24 @@
 import React from 'react';
-
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 const Header = () => {
   return (
-    <div className="navigation container p-0 box-shadow">
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <a href="index.html" className="navbar-brand mb-1">
-          Jalebi Jodi
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarText"
-          aria-controls="navbarText"
-          aria-expanded="false"
-          aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarText">
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0 text-center">
-            <li className="nav-item custom-nav">
-              <a className="nav-link active" aria-current="page" href="#">
-                PROFILE REVIEWS
-              </a>
-            </li>
-            <li className="nav-item custom-nav">
-              <a className="nav-link" href="#">
-                COACHING
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  </div>
-  
-
-  
-
-
+    <Container className='g-0 box-shadow'> 
+      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+    <Container>
+      <Navbar.Brand href="#home">Jalebi Jodi</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav" className='justify-content-end'>
+        <Nav className=''>
+          <Nav.Link href="#deets">PROFILE REVIEWS</Nav.Link>
+          <Nav.Link href="#deets">COACHING</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+  </Container>
   )
 }
 
