ef94957a17902b085a11fc51f1c21ceec29dcedd
Supriya Kankure <supriyakankure@Supriyas-Air.lan>
2023-02-03 14:09:21 -0500

    routing

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <Container className='g-0 box-shadow'> 
       <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
     <Container>
-      <Navbar.Brand href="/">Jalebi Jodi</Navbar.Brand>
+      <Navbar.Brand>
+        <Link to="/"> Jalebi Jodi</Link>
+        </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav" className='justify-content-end'>
         <Nav className=''>
